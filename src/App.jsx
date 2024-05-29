45db13c0f144d283fb30aead42dc4e0d5b8f7bf9
Sanalemba991 <154108599+Sanalemba991@users.noreply.github.com>
2024-05-29 22:45:45 +0530

    configuration Html with google Font Open Sans

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex flex-col p-4 max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+     <Header/>
+       <HomePage/>
+      </section>
+      <h1 className="text-green-400">Hello</h1>
+      <footer></footer>
+    </div>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <header className="flex items-center justify-between gap-4">
+          <h1 className='font-mdeium'>
+            Language
+            <span className="text-blue-400 bold">Translation</span>
+          </h1>
+          <button className="flex items-center gap-2">
+            <p>New</p>
+            <i className="fa-solid fa-plus"></i>
+          </button>
+        </header>
+    </div>
+  )
+}
+
+export default Header
