19f7ee3b408a44057a9b3283ad57fa0d120b4116
Sanalemba991 <154108599+Sanalemba991@users.noreply.github.com>
2024-05-30 19:53:30 +0530

    Main Page Ui full

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,8 @@ function Header() {
             Language
             <span className="text-blue-400 bold">Translation</span>
           </h1>
-          <button className="flex items-center gap-2">
+          <button className="flex items-center gap-2
+          specialBtn px-4 py-2 rounded-lg text-blue-400">
             <p>New</p>
             <i className="fa-solid fa-plus"></i>
           </button>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,11 +6,15 @@ function HomePage() {
     <h1 className='font-semibold text-6xl md:text-7xl'> Language <span className='text-blue-400 bold'>Translation</span></h1>
     <h3 className='font-medium md:text-lg'>Record <span className='text-blue-400'>&rarr;</span> Transcribe <span className='text-blue-400'>&rarr;</span> Translate</h3>
     
-    <button className='flex items-center text-base justify-between gap-4 mx-auto max-w-full my-4'>
-        <p>Record</p>
+    <button className='flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 specialBtn px-4 py-2 rounded-xl'>
+        <p className='text-blue-400'>Record</p>
 
     <i className="fa-solid fa-microphone"></i>
     </button>
+    <p className='text-base'> Or  <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-150'>
+      UpLoad <input className='hidden' type='file' accept='.mp3,.wave'>
+      </input></label> a mp3</p>
+      <p className='italic text-slate-500'> Ask Me Anything</p>
      </main>
   
 )
