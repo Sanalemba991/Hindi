cc8496374ead22310e6c43ead5e2b1dc97b1cf8b
Sanalemba991 <154108599+Sanalemba991@users.noreply.github.com>
2024-06-02 20:07:02 +0530

    New COLOR

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
       <header className="flex items-center justify-between gap-4">
        <a href="/">   <h1 className='font-mdeium'>
             Language
-            <span className="text-blue-400 bold">Translation</span>
+            <span className="text-orange-400 bold">Translation</span>
           </h1></a>
         <a  href="/" className="flex items-center gap-2
-          specialBtn px-4 text-sm py-2 rounded-lg text-blue-400">
+          specialBtn px-4 text-sm py-2 rounded-lg text-orange-400">
             <p>New</p>
             <i className="fa-solid fa-plus"></i>
           </a> 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -67,10 +67,10 @@ export default function HomePage(props) {
 
     return (
         <main className='flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4  justify-center pb-20'>
-            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Language<span className='text-blue-400 bold'>Translation</span></h1>
-            <h3 className='font-medium md:text-lg'>Record <span className='text-blue-400'>&rarr;</span> Transcribe <span className='text-blue-400'>&rarr;</span> Translate</h3>
+            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Language<span className='text-orange-400 bold'>Translation</span></h1>
+            <h3 className='font-medium md:text-lg'>Record <span className='text-orange-400'>&rarr;</span> Transcribe <span className='text-orange-400'>&rarr;</span> Translate</h3>
             <button onClick={recordingStatus === 'recording' ? stopRecording : startRecording} className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
-                <p className='text-blue-400'>{recordingStatus === 'inactive' ? 'Record' : `Stop recording`}</p>
+                <p className='text-orange-400'>{recordingStatus === 'inactive' ? 'Record' : `Stop recording`}</p>
                 <div className='flex items-center gap-2'>
                     {/* {duration !== 0 && (
                         <p className='text-sm'>{duration}s</p>
@@ -78,7 +78,7 @@ export default function HomePage(props) {
                     <i className={"fa-solid duration-200 fa-microphone " + (recordingStatus === 'recording' ? ' text-rose-300' : "")}></i>
                 </div>
             </button>
-            <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>upload <input onChange={(e) => {
+            <p className='text-base'>Or <label className='text-orange-400 cursor-pointer hover:text-orange-600 duration-200'>upload <input onChange={(e) => {
                 const tempFile = e.target.files[0]
                 setFile(tempFile)
             }} className='hidden' type='file' accept='.mp3,.wave' /></label> a mp3 file</p>
